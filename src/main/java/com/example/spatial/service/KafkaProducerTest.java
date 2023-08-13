255f4962a72e260400de41c1fb4fa106120a0a54
amr.halim@afaqy.com <amr.halim@afaqy.com>
2023-08-13 11:19:48 +0300

    fix

--- src/main/java/com/example/spatial/service/KafkaProducerTest.java
+++ src/main/java/com/example/spatial/service/KafkaProducerTest.java
@@ -1,4 +1,21 @@
-package com.example.spatial.producer;
-
-public class KafkaProducerTest {
-}
+//package com.example.spatial.service;
+//
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class KafkaProducerTest {
+//
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//    private final String topicName = "test-topic";
+//
+//    public KafkaProducerTest(KafkaTemplate<String, String> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+//
+//    public void produceMessage(String message) {
+//        kafkaTemplate.send(topicName, message);
+//        System.out.println("Produced message: " + message);
+//    }
+//
+//}
